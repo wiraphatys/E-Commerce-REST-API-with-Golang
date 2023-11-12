44d1d9af59afa1697f0f1070e8b1abed81977047
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2023-11-12 15:40:30 +0700

    finished :sparkles: middlewares section
    completed all of middlewares

---
+++ modules/middlewares/middlewaresUsecases/middlewaresUsecases.go
@@ -0,0 +1,16 @@
+package middlewaresUsecases
+
+import "github.com/wiraphatys/E-Commerce-REST-API-with-Golang/modules/middlewares/middlewaresRepositories"
+
+type IMiddlewaresUsecase interface {
+}
+
+type middlewaresUsecase struct {
+	middlewaresRepository middlewaresRepositories.IMiddlewaresRepository
+}
+
+func MiddlewaresUsecase(middlewaresRepository middlewaresRepositories.IMiddlewaresRepository) IMiddlewaresUsecase {
+	return &middlewaresUsecase{
+		middlewaresRepository: middlewaresRepository,
+	}
+}
--- modules/servers/module.go
+++ modules/servers/module.go
@@ -2,6 +2,9 @@ package servers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/wiraphatys/E-Commerce-REST-API-with-Golang/modules/middlewares/middlewaresHandlers"
+	"github.com/wiraphatys/E-Commerce-REST-API-with-Golang/modules/middlewares/middlewaresRepositories"
+	"github.com/wiraphatys/E-Commerce-REST-API-with-Golang/modules/middlewares/middlewaresUsecases"
 	"github.com/wiraphatys/E-Commerce-REST-API-with-Golang/modules/monitor/monitorHandlers"
 )
 
@@ -10,15 +13,24 @@ type IModuleFactory interface {
 }
 
 type moduleFactory struct {
-	r fiber.Router
-	s *server
+	r   fiber.Router
+	s   *server
+	mid middlewaresHandlers.IMiddlewaresHandler
 }
 
-func InitModule(r fiber.Router, s *server) IModuleFactory {
+func InitModule(r fiber.Router, s *server, mid middlewaresHandlers.IMiddlewaresHandler) IModuleFactory {
 	return &moduleFactory{
-		r: r,
-		s: s,
+		r:   r,
+		s:   s,
+		mid: mid,
 	}
+}
+
+func InitMiddlewares(s *server) middlewaresHandlers.IMiddlewaresHandler {
+	repository := middlewaresRepositories.MiddlewaresRepository(s.db)
+	usecase := middlewaresUsecases.MiddlewaresUsecase(repository)
+	handler := middlewaresHandlers.MiddlewaresHandler(s.cfg, usecase)
+	return handler
 }
 
 func (m *moduleFactory) MonitorModule() {
